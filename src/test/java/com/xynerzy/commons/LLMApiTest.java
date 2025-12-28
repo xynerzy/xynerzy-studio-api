622fe5733c53fb2f42c32c918bf51fcd728223f6
coder <coder@email.com>
2025-12-29 00:34:10 +0900

    Ollama LLM Chat Test

--- src/test/java/com/xynerzy/commons/LLMApiTest.java
+++ src/test/java/com/xynerzy/commons/LLMApiTest.java
@@ -21,6 +21,7 @@ import com.xynerzy.commons.TestUtil.TestLevel;
 import com.xynerzy.commons.llm.LLMApiBase;
 import com.xynerzy.commons.llm.LLMApiGemini;
 import com.xynerzy.commons.llm.LLMApiGeminiOAuth2;
+import com.xynerzy.commons.llm.LLMApiOllama;
 import com.xynerzy.commons.llm.LLMApiOpenAI;
 import com.xynerzy.commons.llm.LLMProperties;
 
@@ -28,9 +29,8 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class LLMApiTest {
-  @Test void testOpenAIApi() throws InterruptedException {
+  @Test void testOpenAIApi() throws Exception {
     if (!TestUtil.isEnabled("testOpenAIApi", TestLevel.MANUAL)) { return; }
-    /* Arrange */
     LLMProperties props = new LLMProperties();
     /* "http://localhost:1234/v1" */
     props.setBaseUrl(System.getenv("OPENAI_API_BASE_URL"));
@@ -38,30 +38,31 @@ public class LLMApiTest {
     props.setModel(System.getenv("OPENAI_API_MODEL"));
     props.setApiKey(System.getenv("OPENAI_API_KEY"));
 
-    WebClient.Builder webClientBuilder = WebClient.builder();
-    LLMApiBase api = new LLMApiOpenAI(props, webClientBuilder);
+    WebClient.Builder wbldr = WebClient.builder();
+    LLMApiBase api = new LLMApiOpenAI(props, wbldr);
 
     String request = "Hello? Who are you?";
+    
     StringBuilder resp = new StringBuilder();
     LinkedBlockingQueue<Boolean> latch = new LinkedBlockingQueue<>();
     AtomicReference<Throwable> error = new AtomicReference<>();
-    /* Act & Assert */
-    log.info("Sending request to LM-Studio...");
+    /* Act */
+    log.info("Sending request to Open-AI...");
     api.streamChat(
-        request,
-        chunk -> {
-          log.debug("Received chunk: {}", chunk);
-          if ("\0\0".equals(chunk)) {
-            latch.add(Boolean.TRUE);
-          }
-          resp.append(chunk);
-        },
-        () -> log.info("Stream completed."),
-        err -> {
-          log.error("Stream failed with an error", err);
-          error.set(err);
+      request,
+      chunk -> {
+        log.debug("Received chunk: {}", chunk);
+        if ("\0\0".equals(chunk)) {
           latch.add(Boolean.TRUE);
         }
+        resp.append(chunk);
+      }, () -> {
+        log.info("Stream completed.");
+      }, err -> {
+        log.error("Stream failed with an error", err);
+        error.set(err);
+        latch.add(Boolean.TRUE);
+      }
     );
     /**
      * Since the actual streamChat implementation is invoked asynchronously via subscribe(), 
@@ -77,21 +78,21 @@ public class LLMApiTest {
     log.info("Final Response: {}", resp.toString());
   }
 
-  @Test void testGeminiApi() throws InterruptedException {
+  @Test void testGeminiApi() throws Exception {
     if (!TestUtil.isEnabled("testGeminiApi", TestLevel.MANUAL)) { return; }
     LLMProperties props = new LLMProperties();
     props.setApiKey(System.getenv("GEMINI_API_KEY"));
+    /* "gemini-2.5-flash" */
     props.setModel(System.getenv("GEMINI_API_MODEL"));
 
-    WebClient.Builder webClientBuilder = WebClient.builder();
-    LLMApiBase api = new LLMApiGemini(props, webClientBuilder);
+    WebClient.Builder wbldr = WebClient.builder();
+    LLMApiBase api = new LLMApiGemini(props, wbldr);
 
     String request = "Hello? Who are you?";
 
     StringBuilder resp = new StringBuilder();
     LinkedBlockingQueue<Boolean> latch = new LinkedBlockingQueue<>();
     AtomicReference<Throwable> error = new AtomicReference<>();
-
     /* Act */
     log.info("Sending request to Gemini API...");
     api.streamChat(
@@ -123,33 +124,77 @@ public class LLMApiTest {
     props.setClientId(System.getenv("GEMINI_API_CLIENT_ID"));
     props.setClientSecret(System.getenv("GEMINI_API_CLIENT_SECRET"));
     props.setRefreshToken(System.getenv("GEMINI_API_REFRESH_TOKEN"));
+    /* "gemini-2.5-flash" */
     props.setModel(System.getenv("GEMINI_API_MODEL"));
-    WebClient.Builder webClientBuilder = WebClient.builder();
-    LLMApiBase api = new LLMApiGeminiOAuth2(props, webClientBuilder);
+    WebClient.Builder wbldr = WebClient.builder();
+    LLMApiBase api = new LLMApiGeminiOAuth2(props, wbldr);
 
     String request = "Hello? Who are you?";
 
+    StringBuilder resp = new StringBuilder();
     LinkedBlockingQueue<Boolean> latch = new LinkedBlockingQueue<>();
-    StringBuilder responseBuilder = new StringBuilder();
     AtomicReference<Throwable> error = new AtomicReference<>();
-    
     /* Act */
     log.info("Sending request to Gemini API with OAuth2...");
-    api.streamChat(request, chunk -> {
-      log.debug("Received chunk: {}", chunk);
-      responseBuilder.append(chunk);
-    }, () -> {
-      log.info("Stream completed.");
-      latch.add(Boolean.TRUE);
-    }, err -> {
-      log.error("Stream failed with an error", err);
-      error.set(err);
-      latch.add(Boolean.TRUE);
-    });
+    api.streamChat(
+      request,
+      chunk -> {
+        log.debug("Received chunk: {}", chunk);
+        resp.append(chunk);
+      }, () -> {
+        log.info("Stream completed.");
+        latch.add(Boolean.TRUE);
+      }, err -> {
+        log.error("Stream failed with an error", err);
+        error.set(err);
+        latch.add(Boolean.TRUE);
+      }
+    );
     /* Assert */
     while (latch.poll(1000, TimeUnit.MILLISECONDS) == null) { }
     assertNull(error.get(), "Stream should complete without errors.");
-    assertFalse(responseBuilder.toString().isEmpty(), "Response should not be empty.");
-    log.info("Final Response: {}", responseBuilder.toString());
+    assertFalse(resp.toString().isEmpty(), "Response should not be empty.");
+    log.info("Final Response: {}", resp.toString());
+  }
+
+  @Test void testOllamaApi() throws InterruptedException {
+    if (!TestUtil.isEnabled("testOllamaApi", TestLevel.MANUAL)) { return; }
+    LLMProperties props = new LLMProperties();
+    /* "http://localhost:11434" */
+    props.setBaseUrl(System.getenv("OLLAMA_API_BASE_URL"));
+    /* "gpt-oss:20b" */
+    props.setModel(System.getenv("OLLAMA_API_MODEL"));
+
+    WebClient.Builder wbldr = WebClient.builder();
+    LLMApiOllama api = new LLMApiOllama(props, wbldr);
+
+    String request = "Hello? Who are you?";
+
+    StringBuilder resp = new StringBuilder();
+    LinkedBlockingQueue<Boolean> latch = new LinkedBlockingQueue<>();
+    AtomicReference<Throwable> error = new AtomicReference<>();
+    /* Act */
+    log.info("Sending request to Ollama API...");
+    api.streamChat(
+        request,
+        chunk -> {
+          log.debug("Received chunk: {}", chunk);
+          resp.append(chunk);
+        },
+        () -> {
+          log.info("Stream completed.");
+          latch.add(Boolean.TRUE);
+        },
+        err -> {
+          log.error("Stream failed with an error", err);
+          error.set(err);
+          latch.add(Boolean.TRUE);
+        }
+    );
+    /* Assert */
+    while (latch.poll(1000, TimeUnit.MILLISECONDS) == null) { }
+    assertNull(error.get(), "Stream should complete without errors.");
+    assertFalse(resp.toString().isEmpty(), "Response should not be empty.");
+    log.info("Final Response: {}", resp.toString());
   }
 }
